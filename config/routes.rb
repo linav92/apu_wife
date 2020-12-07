2b80c346a096b4c288c5ba10a1a2a53ea0ecf889
Lina Vallejo <linav92@gmail.com>
2020-12-06 23:35:55 -0300

    Created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :breastfeedings
   resources :babies
+
+  root to: "babies#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
